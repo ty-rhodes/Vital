b434797f39909ea67088b55e294f6770ab3484f4
ty-rhodes <104220819+ty-rhodes@users.noreply.github.com>
2024-06-24 19:12:14 -0400

    added empty states to chart views

--- Vital/Views/Lists/HeartRateListData.swift
+++ Vital/Views/Lists/HeartRateListData.swift
@@ -16,14 +16,18 @@ struct HeartRateListData: View {
     
     var body: some View {
         VStack {
-            List(MockData.heartrates) { data in
-                HStack {
-                    Text(data.date.displayFormat)
-                    Spacer()
-                    Text("\(data.value, format: .number.precision(.fractionLength(0))) bpm")
+            if hkManager.heartRateData.isEmpty {
+                ChartEmptyView(systemImageName: "waveform.path.ecg", title: "No Data", description: "There is no resting heart rate data to pull from the Health App")
+            } else {
+                List(hkManager.heartRateData.reversed()) { data in
+                    HStack {
+                        Text(data.date.displayFormat)
+                        Spacer()
+                        Text("\(data.value, format: .number.precision(.fractionLength(0))) bpm")
+                    }
                 }
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
             }
-            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
         }
         .padding(.horizontal)
         .listStyle(.inset)
