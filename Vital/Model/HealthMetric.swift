a418edc7e901b00c8a704251ea8bf04199e17186
ty-rhodes <104220819+ty-rhodes@users.noreply.github.com>
2024-07-02 11:33:54 -0400

    added health standards within account view

--- Vital/Model/HealthMetric.swift
+++ Vital/Model/HealthMetric.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct HealthMetric: Identifiable {
+struct HealthMetric: Identifiable, Hashable {
     let id = UUID()
     let date: Date
     let value: Double
--- Vital/Screens/AccountView.swift
+++ Vital/Screens/AccountView.swift
@@ -12,6 +12,41 @@ struct AccountView: View {
         NavigationStack {
             VStack {
                 Form {
+                    Section(header: Label("General Health Info", systemImage: "scroll")
+                        .foregroundStyle(.secondary)) {
+                            HStack {
+                                Symbols.link
+                                VStack(alignment: .leading) {
+                                    Link("Heart Health", destination: URL(string: "https://www.heart.org/en/healthy-living/fitness/fitness-basics/target-heart-rates")!)
+                                        .font(.system(size: 18, weight: .medium))
+                                    Text("Maximum and target heart rate by age.")
+                                        .font(.system(size: 14, weight: .light))
+                                }
+                            }
+                            
+                            HStack {
+                                Symbols.link
+                                VStack(alignment: .leading) {
+                                    Link("Importance of Daily Steps", destination: URL(string: "https://www.nih.gov/news-events/nih-research-matters/number-steps-day-more-important-step-intensity")!)
+                                        .font(.system(size: 18, weight: .medium))
+                                    Text("The low cost of daily physical activity.")
+                                        .font(.system(size: 14, weight: .light))
+                                }
+                            }
+                            
+                            HStack {
+                                Symbols.link
+                                VStack(alignment: .leading) {
+                                    Link("Calories Burned by Age", destination: URL(string: "https://health.clevelandclinic.org/calories-burned-in-a-day")!)
+                                        .font(.system(size: 18, weight: .medium))
+                                    Text("Average calories burned by age.")
+                                        .font(.system(size: 14, weight: .light))
+                                }
+                            }
+
+                        }
+                        .foregroundStyle(.primary)
+                    
                     Section(header: Label("Legal", systemImage: "scroll")
                         .foregroundStyle(.secondary)) {
                             HStack {
@@ -23,7 +58,6 @@ struct AccountView: View {
                                 Link("Privacy Policy", destination: URL(string: "https://sites.google.com/view/ritualbrewandrenew/home")!)
                             }
                         }
-                        .font(.system(size: 16, weight: .light))
                         .foregroundStyle(.primary)
                 }
             }
