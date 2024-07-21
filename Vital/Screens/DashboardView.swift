caaad1ec49163066db430774a827eb9f44ce6587
ty-rhodes <104220819+ty-rhodes@users.noreply.github.com>
2024-07-21 17:56:47 -0400

    changed color of date display in dashboard view

--- Vital/Screens/DashboardView.swift
+++ Vital/Screens/DashboardView.swift
@@ -46,6 +46,7 @@ struct DashboardView: View {
                         // MARK: - Date Display
                         HStack {
                             Text(Date().displayFormat)
+                                .foregroundStyle(.secondary)
                             Spacer()
                         }
                         .padding(.horizontal)
